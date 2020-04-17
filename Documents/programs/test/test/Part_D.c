9b2ace6ef8c0ec89e7ecd84b93ca0837fddc311d
retinderdeep singh <retinderdeepsingh@192.168.1.3>
2020-04-17 18:33:14 +0530

    first commit

---
+++ Documents/programs/test/test/Part_D.c
@@ -0,0 +1,38 @@
+//
+//  main.c
+//  test
+//
+//  Created by retinderdeep singh on 17/04/20.
+//  Copyright © 2020 retinderdeep singh. All rights reserved.
+//
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+    
+    FILE *fp;
+    char ch;
+    int num;
+    long length;
+    
+    printf("Enter the value of num : ");
+    scanf("%d", &num);
+    
+    fp = fopen("test.txt", "r");
+    if (fp == NULL) {
+        puts("cannot open this file");
+        exit(1);
+    }
+    
+    fseek(fp, 0, SEEK_END);
+    length = ftell(fp);
+    fseek(fp, (length - num), SEEK_SET);
+    
+    do {
+        ch = fgetc(fp);
+        putchar(ch);
+    } while (ch != EOF);
+    
+    fclose(fp);
+    return(0);
+}
